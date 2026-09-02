35ffaadf0ca49a400c85f98727e44b5bf48e87a0
ShortCodes19 <jumak4863@gmail.com>
2026-09-02 17:44:19 +0500

    started the server

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
-const PORT = 4000;
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Job Tracker API is running");
+});
+
+app.post("/api/jobs", (req, res) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    message: "Job received",
+    job: req.body,
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
