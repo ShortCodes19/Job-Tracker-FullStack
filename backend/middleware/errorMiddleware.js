a5c4143f268db68d106425a7734fba30aa2eeca6
ShortCodes19 <jumak4863@gmail.com>
2026-09-07 09:15:38 +0500

    fixed the error middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,6 +1,21 @@
 const errorMiddleware = (err, req, res, next) => {
   console.log(err);
 
+  if (typeof err === "string") {
+    return res.status(400).json({
+      message: err,
+    });
+  }
+
+  if (err.name === "ValidationError") {
+    return res.status(400).json({
+      message: "Invalid job data",
+      errors: Object.values(err.errors).map(
+        (validationError) => validationError.message,
+      ),
+    });
+  }
+
   res.status(500).json({
     message: "Something went wrong!",
   });
